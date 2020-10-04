e5ca66776a61d90c6ff47e1cdcbf916a972c6afc
Gerolf Ziegenhain <gerolfziegenhain@DFS-Gerolf-MacBook-8.local>
2020-10-04 14:04:27 +0200

    log

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-/usr/bin/python3  /home/pi/moonboard/run.py --led_layout=evo --debug --driver SimPixel $1 2>&1 > /var/log/moonboard
+set -o errexit
+LOG_FILE=/var/log/moonboard
+
+exec 1>$LOG_FILE
+exec 2>&1
+
+/usr/bin/python3  /home/pi/moonboard/run.py --led_layout=evo --debug --driver SimPixel $1 
