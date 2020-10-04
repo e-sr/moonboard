923693c085a741cddfc6ed4d9b99010add74dcf9
Gerolf Ziegenhain <gerolfziegenhain@DFS-Gerolf-MacBook-8.local>
2020-10-04 13:41:25 +0200

    bt_adv

--- install.sh
+++ install.sh
@@ -24,5 +24,12 @@ echo "Install DBUS service"
 sudo cp ble/com.moonboard.conf /etc/dbus-1/system.d
 sudo cp ble/com.moonboard.service /usr/share/dbus-1/system-services/
 
+echo "Start advertising stuff"
+sudo hcitool -i hci0 cmd 0x08 0x0008  {adv: 32 byte 0-padded if necessary}
+sudo hcitool -i hci0 cmd 0x08 0x0009 {adv: 32 byte 0-padded if necessary}
+sudo hcitool -i hci0 cmd 0x08 0x0006 {min:2byte} {max:2byte} {connectable:1byte} 00 00 00 00 00 00 00 00 07 00
+sudo hcitool -i hci0 cmd 0x08 0x000a 01
+
+
 #python3 ./run.py --driver SimPixel --debug
 
