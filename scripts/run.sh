3dce8d824b5933544b98413cdcfd1bc3b20a58aa
Gerolf Ziegenhain <gerolfziegenhain@DFS-Gerolf-MacBook-89.local>
2020-10-15 14:09:57 +0200

    WS2811

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,4 +5,4 @@ LOG_FILE=/var/log/moonboard
 exec 1>$LOG_FILE
 exec 2>&1
 
-/usr/bin/python3  /home/pi/moonboard/run.py --led_layout=evo --debug --driver SimPixel $1 
+sudo /usr/bin/python3  /home/pi/moonboard/run.py --led_layout=evo --debug --driver PiWS281x $1 
