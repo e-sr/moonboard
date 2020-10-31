967323c478b6820db80da2888665ede598e11a2e
Gerolf Ziegenhain <gerolfziegenhain@DFS-Gerolf-MacBook-163.local>
2020-10-31 22:05:31 +0100

    holds layout

--- led/create_nth_led_layout.py
+++ led/create_nth_led_layout.py
@@ -8,31 +8,40 @@ LED_SPACING = 3
 print ("Layout 1 - Col")
 led_number = 0
 layout = [[0 for i in range(COLS)] for j in range(ROWS)]  
-for c in range (0,COLS):
-    for r in range(0,ROWS):
+for c in range (0, COLS):
+    for r in range(0, ROWS):
         layout[ROWS-1-r][c] = led_number
-        led_number = led_number+LED_SPACING
+        led_number = led_number + LED_SPACING
 print(layout) 
 
 # Every Row Left>Right Bottom>Up
 print ("Layout 2 - Row")
 led_number = 0
 layout = [[0 for i in range(COLS)] for j in range(ROWS)]  
-for r in range(0,ROWS):
-    for c in range (0,COLS):
+for r in range(0, ROWS):
+    for c in range (0, COLS):
         layout[ROWS-1-r][c] = led_number
-        led_number = led_number+LED_SPACING
+        led_number = led_number + LED_SPACING
 print(layout) 
 
 # ZigZag: Left,Bottom>Left,Up >> 1 Right&Up>Down   -- aka: Evo
 print ("Layout 3 - ZigZag")
 led_number = 0
 layout = [[0 for i in range(COLS)] for j in range(ROWS)]  
-for c in range (0,COLS):
-    for r in range(0,ROWS):
+for c in range (0, COLS):
+    for r in range(0, ROWS):
         if (c %2) == 0:
             layout[ROWS-1-r][c] = led_number
         else:
             layout[r][c] = led_number
-        led_number = led_number+LED_SPACING
-print(layout) 
+        led_number = led_number + LED_SPACING
+print(layout)
+
+# Hold Layout
+print ("Layout Holds")
+layout = [[0 for i in range(COLS)] for j in range(ROWS)]  
+for c in range (0, COLS):
+    for r in range(0, ROWS):
+        hold = (chr(c+65)+str(r+1))
+        layout[ROWS-1-r][c] = hold
+print (layout)
