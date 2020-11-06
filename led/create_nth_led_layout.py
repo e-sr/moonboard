d0ad6d4fef8722a0f817e35985fc22dbbc842d5c
Gerolf Ziegenhain <gerolfziegenhain@DFS-Gerolf-MacBook-163.local>
2020-11-06 23:17:41 +0100

    layout

--- led/create_nth_led_layout.py
+++ led/create_nth_led_layout.py
@@ -1,28 +1,28 @@
 #!/usr/bin/python
 # This script is used to create the arrays for the LED layouts
-ROWS = 18
+ROWS = 18 *3
 COLS = 11
-LED_SPACING = 3
+LED_SPACING = 1
 
 # Every Col Bottom>Up Left>Right
-print ("Layout 1 - Col")
+#print ("Layout 1 - Col")
 led_number = 0
 layout = [[0 for i in range(COLS)] for j in range(ROWS)]  
 for c in range (0, COLS):
     for r in range(0, ROWS):
         layout[ROWS-1-r][c] = led_number
         led_number = led_number + LED_SPACING
-print(layout) 
+#print(layout) 
 
 # Every Row Left>Right Bottom>Up
-print ("Layout 2 - Row")
+#print ("Layout 2 - Row")
 led_number = 0
 layout = [[0 for i in range(COLS)] for j in range(ROWS)]  
 for r in range(0, ROWS):
     for c in range (0, COLS):
         layout[ROWS-1-r][c] = led_number
         led_number = led_number + LED_SPACING
-print(layout) 
+#print(layout) 
 
 # ZigZag: Left,Bottom>Left,Up >> 1 Right&Up>Down   -- aka: Evo
 print ("Layout 3 - ZigZag")
@@ -38,7 +38,7 @@ for c in range (0, COLS):
 print(layout)
 
 # Hold Layout
-print ("Layout Holds")
+#print ("Layout Holds")
 layout = [[0 for i in range(COLS)] for j in range(ROWS)]  
 for c in range (0, COLS):
     for r in range(0, ROWS):
@@ -48,7 +48,7 @@ for c in range (0, COLS):
 
 
 # List of all holds
-print ("List of all holds")
+#print ("List of all holds")
 layout = [0 for i in range(COLS*ROWS)]
 for c in range (0, COLS):
     for r in range(0, ROWS):
