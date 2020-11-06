e575eee7c2f360be5ff593601df77fc388696e89
Gerolf Ziegenhain <gerolfziegenhain@DFS-Gerolf-MacBook-163.local>
2020-11-07 00:53:55 +0100

    test

--- led/moonboard.py
+++ led/moonboard.py
@@ -114,7 +114,7 @@ class MoonBoard:
     def set_hold(self, hold, color=DEFAULT_COLOR):
         x_grid_name, y_grid_name = hold[0], int(hold[1:])
         x = self.X_GRID_NAMES.index(x_grid_name)
-        y = (self.ROWS -2 - y_grid_name * self.LED_SPACING    +   x*self.LED_SPACING ) -3 # FIXME
+        y = (self.ROWS - y_grid_name * self.LED_SPACING    +   x*self.LED_SPACING )  # FIXME
         print (x, y)
         self.layout.set(x, y, color)
 
