22a2872aac63e4a2c632b138fcfe348b7a521606
Gerolf Ziegenhain <gerolfziegenhain@DFS-Gerolf-MacBook-163.local>
2020-11-07 20:00:18 +0100

    wip

--- led/create_nth_led_layout.py
+++ led/create_nth_led_layout.py
@@ -35,11 +35,13 @@ led_number = 0
 layout = [[0 for i in range(COLS)] for j in range(ROWS)]  
 for c in range (0, COLS):
     for r in range(0, ROWS):
+        hold = ""
         if (c %2) == 0:
             layout[ROWS-1-r][c] = led_number
+            hold = (chr(c+65)+str(ROWS-r))
         else:
             layout[r][c] = led_number
-        hold = (chr(c+65)+str(r+1))
+            hold = (chr(c+65)+str(r+1))
         #print (hold, c,r,led_number)
         MAPPING [hold] = led_number
         led_number = led_number + LED_SPACING
