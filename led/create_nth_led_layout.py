1ec68944f6408c91c12173951abcf97891a618fd
Gerolf Ziegenhain <gerolfziegenhain@DFS-Gerolf-MacBook-163.local>
2020-11-07 19:41:38 +0100

    wip

--- led/create_nth_led_layout.py
+++ led/create_nth_led_layout.py
@@ -45,7 +45,7 @@ for c in range (0, COLS):
         led_number = led_number + LED_SPACING
         if (hold == "E13"):
             led_number = led_number + 1
-        if (hold == "I10"):
+        if (hold == "I9"):
             led_number = led_number + 1
 
 #print(layout)
