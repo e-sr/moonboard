aeb90179ac13f2d35717f69e1d70cc8d88090e28
Lidia Parrilla <lidia_alkala_94@hotmail.com>
2021-02-27 12:22:43 +0100

    Changed intermediate led color from orange o blue

--- led/moonboard.py
+++ led/moonboard.py
@@ -57,7 +57,7 @@ LED_LAYOUT = {
 
 
 class MoonBoard:
-    DEFAULT_PROBLEM_COLORS = {'START': COLORS.green, 'TOP': COLORS.red, 'MOVES': COLORS.orange}
+    DEFAULT_PROBLEM_COLORS = {'START': COLORS.green, 'TOP': COLORS.red, 'MOVES': COLORS.blue}
     DEFAULT_COLOR = COLORS.blue  # FIXME ?
     X_GRID_NAMES = string.ascii_uppercase[0:11]  # FIXME: del
     LED_SPACING = 1  # Use every n-th LED only - used for 3 x 4x5 LED strp      # FIXME: normal=1
