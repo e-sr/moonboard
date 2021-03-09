3f62cc6f6ff112ed4b2fdf48a414c75189a771de
Lidia Parrilla <75931652+lidiapb@users.noreply.github.com>
2021-03-09 22:24:57 +0100

    Update to install.sh to add GPIO dependences for the new reset button

--- install/install.sh
+++ install/install.sh
@@ -12,7 +12,7 @@ sudo apt-get update
 sudo apt-get upgrade
 
 echo "Install + build led drivers"
-sudo apt-get -y install git vim python3-pip gcc make build-essential
+sudo apt-get -y install git vim python3-pip python3-rpi.gpio gcc make build-essential
 sudo apt-get -y install libatlas-base-dev 
 sudo apt-get -y install python-dev swig scons # for building WS2811 drivers
 
