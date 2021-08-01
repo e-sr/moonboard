ca60d427efc1abe2b6ccb5a4283385cd2ce5743f
Gerolf Ziegenhain <gerolfziegenhain@gz-dfs-macbook.mainz.ziegenhain.com>
2021-08-01 11:19:05 +0200

    fix script

--- scripts/fix_startup.sh
+++ scripts/fix_startup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 sudo systemctl stop moonboard
+sudo systemctl stop com.moonboard
 sudo systemctl restart com.moonboard
-sudo /usr/bin/python3  /home/pi/moonboard/run.py --debug --driver PiWS281x 
+sudo systemctl restart moonboard
+
+#sudo /usr/bin/python3  /home/pi/moonboard/run.py --debug --driver PiWS281x 
