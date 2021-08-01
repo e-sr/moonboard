07d25236fc22a6d420734108454bdce21676f804
Gerolf Ziegenhain <gerolfziegenhain@gz-dfs-macbook.mainz.ziegenhain.com>
2021-08-01 10:34:36 +0200

    fix spacing stuff

--- led/moonboard.py
+++ led/moonboard.py
@@ -60,8 +60,7 @@ class MoonBoard:
     DEFAULT_PROBLEM_COLORS = {'START':COLORS.blue,'TOP':COLORS.red,'MOVES':COLORS.green}
     DEFAULT_COLOR = COLORS.blue #FIXME ?
     X_GRID_NAMES = string.ascii_uppercase[0:11] # FIXME: del
-    LED_SPACING = 3 # Use every n-th LED only - used for 3 x 4x5 LED strp      # FIXME: normal=1
-    ROWS = 18 * LED_SPACING # FIXME
+    ROWS = 18
     COLS = 11
     NUM_PIXELS = ROWS*COLS
     DEFAULT_BRIGHTNESS = 100 # FIXME: to config file
@@ -145,7 +144,7 @@ class MoonBoard:
     def run_animation(self, run_options={}, **kwds): # FIXME: will it still work?
         duration = 0.001
 
-        for i in range(1,self.ROWS-2):
+        for i in range(1,self.ROWS):
             for j in range (0,self.COLS):
 
                 le = chr(j+65)
